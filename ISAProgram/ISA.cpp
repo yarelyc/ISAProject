bfb1c019e0c69daba8085902492ecf29e9d4a670
celi19 <araceligopar@student.hartnell.edu>
2015-12-03 19:36:00 -0800

    Read from file

--- ISAProgram/ISA.cpp
+++ ISAProgram/ISA.cpp
@@ -3,7 +3,7 @@
 //
 // Summary: This file
 //
-// Author: Yarely Chino & Aracely
+// Author: Yarely Chino & Araceli
 // Created: 12/ 01/2015
 // Modifications:
 //
@@ -11,10 +11,29 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
 using namespace std;
-
+void loadInstructions(ifstream& file);
 int main(){
 
+        ifstream file;
+        file.open( "file.txt");
+        loadInstructions(file);
+
     return EXIT_SUCCESS;
+}
+void loadInstructions(ifstream& file)
+{
+
+        string row;
+         while ( !file.eof() && getline(file, row) != "STOP" ){
+
+             getline(file, row );
+             cout << row << endl;
+
+         }
+
+        file.close();
+
 }
